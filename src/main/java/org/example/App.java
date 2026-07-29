62dcd8fcd14299650bc3f1414f8d447095a70b05
AlexandrRofficial <alexandr.rofficial@gmail.com>
2026-07-29 20:07:43 +0300

    MavenProject

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class App {
+    public static void main(String[] args) {
+        Person person = new Person("Oleksandr", "Rybalka");
+
+        Gson gson = new GsonBuilder().create();
+
+        String json = gson.toJson(person);
+
+        System.out.println(json);
+    }
+}
+
+class Person {
+    private String name;
+    private String lastName;
+
+    public Person(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
